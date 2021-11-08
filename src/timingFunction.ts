9642cffabef0f828b26e0ab3d8c769a139fbdb96
DmitryScaletta <dmitryscaletta@mail.ru>
2021-11-08 16:36:47 +0300

    🩹 Change timingFunction

--- src/timingFunction.ts
+++ src/timingFunction.ts
@@ -3,11 +3,6 @@ const tpmt = (x: number) => {
   return (Math.pow(2, -10 * x) - 0.0009765625) * 1.0009775171065494;
 }
 
-// https://github.com/d3/d3-ease/blob/main/src/exp.js
-const expOut = (t: number) => {
-  return 1 - tpmt(t);
-}
-
-const timingFunction = expOut;
+const timingFunction = tpmt;
 
 export default timingFunction;
